c637374b84890ce3c4a27e6cf120451029f8cd29
osuossu <osuossu@yuya-no-MacBook-Air.local>
2018-07-12 23:34:09 +0900

    heroku ok

--- mythree/settings.py
+++ mythree/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 ###
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = None #os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
