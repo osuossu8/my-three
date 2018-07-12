fa08742acfe990959af844a62adbae74030b8579
osuossu <osuossu@yuya-no-MacBook-Air.local>
2018-07-12 23:21:40 +0900

    heroku ok

--- mythree/settings.py
+++ mythree/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 
 ###
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
