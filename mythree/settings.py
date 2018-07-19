b246aa4e37513e3c39edcdabe9c8b315269f4329
osuossu <osuossu@yuya-no-MacBook-Air.local>
2018-07-19 19:52:23 +0900

    heroku ok

--- mythree/settings.py
+++ mythree/settings.py
@@ -163,6 +163,6 @@ STATIC_ROOT = 'static' #os.path.join(BASE_DIR, 'static')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ###
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
